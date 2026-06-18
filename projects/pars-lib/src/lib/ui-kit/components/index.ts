b678f257fbbd89acccb55e711a29bb7d1139ae18
saeed dalvand <saeeddalvand710@gmail.com>
2026-06-18 13:08:58 +0330

    remove  ng_select

--- projects/pars-lib/src/lib/ui-kit/components/index.ts
+++ projects/pars-lib/src/lib/ui-kit/components/index.ts
@@ -2,7 +2,7 @@ export * from './image/image.component';
 export * from './icon/icon.component';
 export * from './basic-card/basic-card.component';
 export * from './label/label.component';
-export * from './select/select.component';
+export * from './select/select';
 export * from './button/button.component';
 export * from './input/input.component';
 export * from './key-val/key-val.component';
